ab7931e3fabbc9753b08904fa37fe321ff05ab57
gwhn <gwhn@weblitz.co.uk>
2016-07-05 14:10:57 +0100

    Created hello-world component. A giant leap for mankind ;)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 
+import { HelloWorldComponent } from './hello-world';
+
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives: [HelloWorldComponent]
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'ng2 Adventure';
 }
---
+++ src/app/hello-world/hello-world.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'hello-world',
+  templateUrl: 'hello-world.component.html',
+  styleUrls: ['hello-world.component.css']
+})
+export class HelloWorldComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {
+  }
+
+}
