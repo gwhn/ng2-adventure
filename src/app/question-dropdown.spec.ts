a54a2ed9df64af8540de81b946df811acf18224a
gwhn <gwhn@weblitz.co.uk>
2016-07-18 11:45:42 +0100

    Created QuestionDropdown class

---
+++ src/app/question-dropdown.spec.ts
@@ -0,0 +1,15 @@
+/* tslint:disable:no-unused-variable */
+
+import {
+  beforeEach, beforeEachProviders,
+  describe, xdescribe,
+  expect, it, xit,
+  async, inject
+} from '@angular/core/testing';
+import {QuestionDropdown} from './question-dropdown';
+
+describe('QuestionDropdown', () => {
+  it('should create an instance', () => {
+    expect(new QuestionDropdown()).toBeTruthy();
+  });
+});
--- src/app/question-textbox.ts
+++ src/app/question-textbox.ts
@@ -3,7 +3,7 @@ import { QuestionBase } from './question-base';
 export class QuestionTextbox extends QuestionBase<string> {
   controlType = 'textbox';
   type: string;
-  
+
   constructor(options: {} = {}) {
     super(options);
     this.type = options['type'] || '';
